70d46b71b187ecacc45af762ecbd9b322bcf7dc2
Anwesh43 <anweshthecool0@gmail.com>
2019-04-05 07:22:44 +0530

    adding SILNode class

--- squaresinlineview/src/main/java/com/anwesh/uiprojects/squaresinlineview/SquaresInLineView.kt
+++ squaresinlineview/src/main/java/com/anwesh/uiprojects/squaresinlineview/SquaresInLineView.kt
@@ -131,8 +131,52 @@ class SquaresInLineView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class SILNode(var i : Int, val state : State = State()) {
+
+        private var next : SILNode? = null
+        private var prev : SILNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SILNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSILNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SILNode {
+            var curr : SILNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return  curr
+            }
+            cb()
+            return this
         }
     }
 }
