2a069bac444f32c83a85009082975084b891b6b7
Anwesh43 <anweshthecool0@gmail.com>
2019-04-05 06:54:21 +0530

    adding libraries and constants

--- squaresinlineview/src/main/java/com/anwesh/uiprojects/squaresinlineview/SquaresInLineView.kt
+++ squaresinlineview/src/main/java/com/anwesh/uiprojects/squaresinlineview/SquaresInLineView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.squaresinlineview
 /**
  * Created by anweshmishra on 05/04/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val squares : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
