a03877d598eda925f3fa72778f4a3f8b05c572a3
Anwesh43 <anweshthecool0@gmail.com>
2019-04-05 07:13:13 +0530

    adding Canvas extension method drawSILNode

--- squaresinlineview/src/main/java/com/anwesh/uiprojects/squaresinlineview/SquaresInLineView.kt
+++ squaresinlineview/src/main/java/com/anwesh/uiprojects/squaresinlineview/SquaresInLineView.kt
@@ -31,3 +31,42 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+fun Int.sf() : Float = 1f - 2 * (this % 2)
+
+fun Canvas.drawXY(x : Float, y : Float, cb : (Canvas) -> Unit) {
+    save()
+    translate(x, y)
+    cb(this)
+    restore()
+}
+
+fun Canvas.drawMovingRect(i : Int, xGap : Float, h : Float, scale : Float, p : Paint) {
+    val sc : Float = scale.divideScale(i, squares)
+    drawXY(xGap * i, (h / 2 - xGap / 2) * i.sf() * sc, {
+        drawRect(RectF(0f, 0f, xGap, xGap), p)
+    })
+}
+
+fun Paint.setStyle(minSize : Float) {
+    style = Paint.Style.STROKE
+    strokeWidth = minSize / strokeFactor
+    strokeCap = Paint.Cap.ROUND
+    color = foreColor
+}
+
+fun Canvas.drawSILNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val xGap : Float = (2 * size) / squares
+    drawXY(gap * (i + 1), h / 2, {
+        it.save()
+        it.rotate(90f * sc2)
+        it.drawLine(-size, 0f, size, 0f, paint)
+        it.drawMovingRect(i, xGap, h, sc1, paint)
+        it.restore()
+    })
+}
